52df5e15f0a550dc8395f71eaab3638f633266a6
YahyaNaq <81474635+YahyaNaq@users.noreply.github.com>
2021-05-26 22:43:06 +0500

    Add files via upload

---
+++ dec_bin_oct_hex_conversions.py
@@ -0,0 +1,32 @@
+n=int(input("Please enter the number you want to convert: "))
+
+#Decimal to Binary
+quo=n//2
+rem=n%2
+Bin=str(rem)
+while quo>1:
+    rem=quo%2
+    Bin=str(rem)+Bin
+    quo=quo//2
+Bin=str(quo)+Bin
+print("Binary:", Bin)
+
+#Decimal to Octal
+Q=n//8 
+R=n%8
+Oct=str(R)
+while Q>0:
+    R=Q%8
+    Oct=str(R)+Oct
+    Q=Q//8
+print("Octal:",Oct)
+
+#Decimal to Hexadecimal
+Q=n//16
+R=n%16
+Hex=str(R)
+while Q>0:
+    R=Q%16
+    Hex=str(R)+Hex
+    Q=Q//16
+print("Hexadecimal:",Hex)
