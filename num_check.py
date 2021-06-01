06725f582c7ba907e3251a6adebf14e7c7a2247e
YahyaNaq <81474635+YahyaNaq@users.noreply.github.com>
2021-06-02 00:23:39 +0500

    Create num_check.py

---
+++ num_check.py
@@ -0,0 +1,8 @@
+#Python Program to Check Whether a Number is Positive or Negative
+number=eval(input("Please enter a number: "))
+if number>0:
+    print("Entered number is positive")
+if number<0:
+    print("Entered number is negative")
+if number==0:
+    print("Entered number is neutral")
