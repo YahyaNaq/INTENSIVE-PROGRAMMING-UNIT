d99e7efa02fd5342bd7076de8987ebc9c47866ab
YahyaNaq <81474635+YahyaNaq@users.noreply.github.com>
2021-06-06 06:54:16 +0500

    Added programs purpose

--- swap_values.py
+++ swap_values.py
@@ -1,3 +1,5 @@
+#Python Program to Exchange the Values of Two Numbers WithoutUsing a Temporary Variable
+
 val1=eval(input("Please enter first value: "))
 val2=eval(input("Please enter second value: "))
 val1,val2=val2,val1
