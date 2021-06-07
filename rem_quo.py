ff38eefa7fe735745749abbc57da7533328d7367
YahyaNaq <81474635+YahyaNaq@users.noreply.github.com>
2021-06-07 11:46:43 +0500

    Create rem_quo.py

---
+++ rem_quo.py
@@ -0,0 +1,9 @@
+# Python Program to read two numbers & print their quotient & remainder
+
+num1=eval(input("Please enter first number: "))
+num2=eval(input("Please enter second number: "))
+print("The quotient when num1 is divided by num2 is",num1/num2)
+print("The remainder when num1 is divided by num2 is",num1%num2)
+print()
+print("The quotient when num2 is divided by num1 is",num2/num1)
+print("The remainder when num2 is divided by num1 is",num2%num1)
