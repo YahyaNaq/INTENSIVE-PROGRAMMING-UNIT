8022bb597ba4f8db204d1322a6541dff11830d6d
YahyaNaq <81474635+YahyaNaq@users.noreply.github.com>
2021-06-21 22:38:28 +0500

    Create simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,7 @@
+# Python Program to Compute Simple Interest Given all the Required Values
+
+prinpl=eval(input("Please enter principal: "))
+rate=eval(input("Please enter interest rate: "))
+time=eval(input("Please enter time: "))
+interest=prinpl*(rate/100)*time
+print(interest)
