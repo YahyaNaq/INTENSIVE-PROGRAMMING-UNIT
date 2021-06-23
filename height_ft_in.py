5bd64c4e76422296ae7f0b08936e774c95719037
YahyaNaq <81474635+YahyaNaq@users.noreply.github.com>
2021-06-23 23:11:55 +0500

    Create height_ft_in.py

---
+++ height_ft_in.py
@@ -0,0 +1,6 @@
+# Python Program to Read Height in Centimeters and then Convert the Height to Feet and Inches
+
+ht_cm=eval(input("Please enter height in centimeters: "))
+ht_in=ht_cm/2.54
+ht_ft=ht_in/12
+print("Height in inches:",ht_in,"inches\nHeight in feet:",ht_ft,"feet")
