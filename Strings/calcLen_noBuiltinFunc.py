acf9b4710779968144f2bc1c19bac5521ba06786
YahyaNaq <81474635+YahyaNaq@users.noreply.github.com>
2021-10-04 19:30:20 +0500

    Create calcLen_noBuiltinFunc.py

---
+++ Strings/calcLen_noBuiltinFunc.py
@@ -0,0 +1,7 @@
+# Python Program to Calculate the Length of a String Without Using a Library Function
+
+name=input("Please input your string: ")
+count=0
+for i in name:
+    count+=1
+print("Length of your string is: ",count)
